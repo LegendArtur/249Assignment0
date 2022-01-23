caba0674a132d0e6fe866dc57128839f33a6c61d
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2022-01-23 12:13:42 -0500

    Initial commit for assignment1

---
+++ src/LadderAndSnake.java
@@ -0,0 +1,12 @@
+// -----------------------------------------------------
+// Assignment #1 (or #0)
+// Question: (include question/part number, if applicable)
+// Written by: Artur Gubaidullin 40208924
+// -----------------------------------------------------
+
+
+public class LadderAndSnake {
+
+
+
+}
